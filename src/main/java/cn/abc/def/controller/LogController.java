afbea4a01eaed79f75f3ac12be1cd210b8f1a0fb
dgxwl <1029325214@qq.com>
2019-07-05 16:13:02 +0800

    logback使用演示

---
+++ src/main/java/cn/abc/def/controller/LogController.java
@@ -0,0 +1,24 @@
+package cn.abc.def.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogController {
+
+	final static Logger logger = LoggerFactory.getLogger(LogController.class);
+	
+	@RequestMapping("/log")
+	public String logback() {
+		//级别由低到高, 但配置文件中有声明的最低级为info, 因此trace和debug不会输出
+		logger.trace("I'm trace");
+		logger.debug("I'm debug");
+		logger.info("I'm info");
+		logger.warn("I'm warn");
+		logger.error("I'm error");
+		
+		return "ok";
+	}
+}
